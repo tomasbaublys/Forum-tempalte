017e7f8ddbd7cc359de4690785857f93c0e18b9c
tomasbaublys <tomas@forumcinemashe.lt>
2018-02-06 19:05:15 +0200

    Create replies associated with each thread relations and run test from ReplyTest and ReadThreadTest files

---
+++ tests/Unit/ReplyTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ReplyTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test */
+    function it_has_an_owner()
+    {
+        $reply = factory('App\Reply')->create();
+        $this->assertInstanceOf('App\User', $reply->owner);
+    }
+}
