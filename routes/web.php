87a87a215d8b2b26f6242531a08721fd21126d08
tomasbaublys <tomas@forumcinemashe.lt>
2018-02-13 19:43:21 +0200

    Correct routes.php file

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-// Route::get('/threads', 'ThreadController@index');
-// Route::get('/threads/create', 'ThreadController@create');
-// Route::post('/threads', 'ThreadController@store');
-// Route::get('/threads/{thread}', 'ThreadController@show');
 Route::resource('threads', 'ThreadController');
 Route::post('/threads/{thread}/replies', 'ReplyController@store');
