65c2dc2203c905e599fca5e8bb2447177287632d
tomasbaublys <tomas@forumcinemashe.lt>
2018-03-08 18:28:17 +0200

    a_user_can_filter_threads_according_to_a_tag test working

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Channel;
 use App\Thread;
 use Illuminate\Http\Request;
 
@@ -16,9 +17,14 @@ class ThreadController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($channelSlug = null)
     {
+        if ($channelSlug) {
+            $channelId = Channel::where('slug', $channelSlug)->first()->id;
+            $threads = Thread::where('channel_id', $channelId)->latest()->get();
+        } else {
         $threads = Thread::latest()->get();
+        }
         return view('threads.index', compact('threads'));
     }
 
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
 Auth::routes();
 Route::get('/home', 'HomeController@index');
 Route::get('threads', 'ThreadController@index');
+Route::get('threads/{channel}', 'ThreadController@index');
 Route::get('threads/create', 'ThreadController@create');
 Route::get('threads/{channel}/{thread}', 'ThreadController@show');
 Route::post('threads', 'ThreadController@store');
